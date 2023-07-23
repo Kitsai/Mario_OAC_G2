c33a14a83636d1210293df6d3f8b9628c5650250
Lucas Rocha <102306509+Kitsai@users.noreply.github.com>
2023-07-23 03:53:20 -0300

    comments

--- src/main.asm
+++ src/main.asm
@@ -23,7 +23,7 @@ MAP_POS: .word 0
 FRAME:		.byte	1	#frame seguinte
 
 # MARIO #####################################################################################################
-MARIO_UPGRADE: .byte 1
+MARIO_UPGRADE: .byte 1 # upgrade atual do mario 1 = pqueno 2 = grande
 MARIO_STATE: .byte 0 # estado do mario = sprite, cycle
 MARIO_POS: .word 160,192 # posicao relativa na tela
 MARIO_POS_TILE: .byte 0, 0
@@ -38,19 +38,19 @@ MARIO_SPEED_UP: .byte 0
 MAIN:
 	li s11,1
 	
-GAME_LOOP:	
-	jal KEY_HANDLE
-	beq s11,zero,MAP_SKIP1
+GAME_LOOP:	# loop principal do jogo
+	jal KEY_HANDLE	# funcao que trata input do teclado
+	beq s11,zero,MAP_SKIP1	# verifica se o mapa precisa ser atualizado
 	jal MAP_RENDER
 MAP_SKIP1:
-	#jal MARIO_RENDER
+	jal MARIO_RENDER	# renderiza o mario
 	la t0, FRAME
 	lb t1,0(t0)
 	li t2, FRAME_ADDRS
 	sb t1, 0(t2)
 	not t1,t1
 	andi t1,t1,1
-	sb t1,0(t0)
+	sb t1,0(t0)	#Atualiza a variavel de frame
 	beq s11,zero,MAP_SKIP2
 	li s11,0
 	jal MAP_RENDER
@@ -67,9 +67,9 @@ MAP_RENDER:
 	sw s1,8(sp)
 
 	la a6,MAPA				# carrega o endereco do mapa em a6
-	la t0, MAP_POS
+	la t0, MAP_POS			
 	lw t0,0(t0)
-	add a6,a6,t0
+	add a6,a6,t0			#desloca o mapa conforme a variavel de posicao de mapa
 	li a1,0					# posicao em x
 	li a2,0					# posicao em y
 	la t0,FRAME
@@ -142,9 +142,9 @@ RENDER_LINE:
 # TILE SELECT a0(tile sheet address), a1(tile index)
 ##########################################################
 TILE_SELECT:
-	li t0,256
-	mul t0,t0,a1
-	add a0, a0,t0
+	li t0,TILE_SIZE	# carrega o tamanho de um tile em t0
+	mul t0,t0,a1	#multiplica o indice por esse tamanho
+	add a0, a0,t0 	# e soma no endereco da imagem
 	ret
 	
 ##########################################################
@@ -154,27 +154,27 @@ MARIO_RENDER:
 	addi sp,sp,-4
 	sw ra,0(sp)
 
-	la t0, MARIO_STATE
+	la t0, MARIO_STATE	# carrega o estado atual do mario e pega o endereco da imegem em a0 usando o TILE_SELECT
 	la a0, MARIOS
 	lb a1,0(t0)
 	jal TILE_SELECT
 
-	la t0, MARIO_POS
+	la t0, MARIO_POS # pega a posicao atual pra por no print
 	lw a1,0(t0)
 	lw a2,4(t0)
 
-	la t0,MARIO_UPGRADE
+	la t0,MARIO_UPGRADE	#verifica o upgrade mas a principio n usa
 	lb t1,0(t0)
 	
-	la t0,FRAME
+	la t0,FRAME	# pega o proximo frame que sera o frame que vai ser printado
 	lb a3,0(t0)
 
-	li a4,TILE_HEIGHT
+	li a4,TILE_HEIGHT	#calcula o tamanho do mario
 	mul a4,a4,t1
 	li a5,TILE_LENGTH
 
-	jal RENDER
-
+	jal RENDER	# printa o mario
+	#TO DO - Limpar sombra
 
 	lw ra,0(sp)
 	addi sp,sp,4
@@ -187,43 +187,43 @@ KEY_HANDLE:
 	addi sp,sp,-4
 	sw ra,0(sp)
 
-	jal KEY_POLL
+	jal KEY_POLL	# chama keypoll que vai retornar em a0 o status do teclado e em a1 a tecla pressionada caso tenha
 
 	beq a0,zero,KEY_HANDLE_END
 	li t0,'a'
-	beq a1,t0,KEY_HANDLE_A
+	beq a1,t0,KEY_HANDLE_A	# se a tecla for um `a` vai para o tratamento do a
 	li t0,'d'
-	beq a1,t0,KEY_HANDLE_D
+	beq a1,t0,KEY_HANDLE_D	# se a tecla for um `D` vai para o tratamento do d
 	li t0,'w'
-	beq a1,t0,KEY_HANDLE_W
+	beq a1,t0,KEY_HANDLE_W	# se a tecla for um `W` vai para o tratamento do w
 	li t0,27
-	beq a1,t0,KEY_HANDLE_ESC
+	beq a1,t0,KEY_HANDLE_ESC	# se a tecla for um `ESC` vai para o tratamento do ESC
 	j KEY_HANDLE_END
 
 KEY_HANDLE_A:
-	la t0, MARIO_POS
-	lw t1,0(t0)
-	li t2, MARIO_LIM_L
-	ble t1,t2,HANDLE_A_END
-	addi t1,t1,-4
-	sw t1,0(t0)
+	la t0, MARIO_POS	# carrega o endereco da posicao do mario
+	lw t1,0(t0)			# pega a posicao no eixo x
+	li t2, MARIO_LIM_L	
+	ble t1,t2,HANDLE_A_END	# verifica se chegou no limite do movimento
+	addi t1,t1,-4	
+	sw t1,0(t0)	#atualiza posicao movendo o mario para a esquerda se possivel
 	HANDLE_A_END:
 		j KEY_HANDLE_END
 
 KEY_HANDLE_D:
-	la t0, MARIO_POS
-	lw t1,0(t0)
+	la t0, MARIO_POS	# carrega o endereco da posicao do mario
+	lw t1,0(t0)			# pega a posicao no eixo x
 	li t2, MARIO_LIM_R
-	bgeu t1,t2,HANDLE_D_MAP
-	addi t1,t1,4
+	bgeu t1,t2,HANDLE_D_MAP	# verifica se chegou no limite do movimento
+	addi t1,t1,4	# se nao chegou soma 4 na posicao do mario
 	sw t1,0(t0)
 	j HANDLE_D_END
 
-	HANDLE_D_MAP:
-		la t0, MAP_POS
-		lw t1,0(t0)
-		addi t1,t1,1
-		sw t1,0(t0)
+	HANDLE_D_MAP:	# se esta no limite mexe a posicao atual do mapa
+		la t0, MAP_POS	#Carrega a posicao do mapa # TO DO - possivelmente ver jeito de mudar isso para n pular um tile inteiro
+		lw t1,0(t0)		
+		addi t1,t1,1	#soma 1 na posicao atual do mapa
+		sw t1,0(t0)	# salva nova posicao do mapa
 		li s11,1
 	HANDLE_D_END:
 		j KEY_HANDLE_END
@@ -233,7 +233,7 @@ KEY_HANDLE_W:
 		j KEY_HANDLE_END
 
 KEY_HANDLE_ESC:
-	li a7, 10
+	li a7, 10	# termina o programa se esc for apertado
 	ecall
 
 
@@ -242,16 +242,14 @@ KEY_HANDLE_END:
 	addi sp,sp, 4
 	ret
 	
-
-
 ##########################################################
 # KEY POLLING
 ##########################################################
 KEY_POLL:
-	li t1,0xFF200000
-	lw t0,0(t1)
-	andi a0,t0,1
+	li t1,0xFF200000	# endereco do teclado
+	lw t0,0(t1)			
+	andi a0,t0,1		#status teclado
 	beq a0,zero,KEY_POLL_END
-	lw a1,4(t1)
+	lw a1,4(t1)			# se tiver tecla guarda ela em a1
 KEY_POLL_END:
 	ret
